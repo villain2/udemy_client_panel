8d74e94f93f4dd255b4d7d8ec0a94fdb3806e570
K.C. Hunter <djvillain@hotmail.com>
2019-12-01 03:19:29 -0500

    Completed Lesson 63: Add Client To Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { FlashMessagesModule} from 'angular2-flash-messages';
 
 // Firebase
 import { environment} from '../environments/environment';
@@ -48,7 +49,8 @@ import { ClientService} from './services/client.service';
         AngularFireModule.initializeApp(environment.firebase, 'clientpanel'),
         AngularFirestoreModule,
         AngularFireAuthModule,
-        FormsModule
+        FormsModule,
+        FlashMessagesModule.forRoot()
     ],
   providers: [ClientService],
   bootstrap: [AppComponent]
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -30,4 +30,8 @@ export class ClientService {
 
     return this.clients;
   }
+
+  newClient(client: Client) {
+    this.clientsCollection.add(client);
+  }
 }
