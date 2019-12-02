d435c90aee447f5262e55c77498914ae2204fc67
K.C. Hunter <djvillain@hotmail.com>
2019-12-01 21:28:35 -0500

    Completed Lesson 72: The Settings Page

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -11,9 +11,17 @@ export class SettingsService {
     disableBalanceOnEdit: true
   };
 
-  constructor() { }
+  constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
